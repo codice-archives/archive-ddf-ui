01a6c03a53d286349c35bee2c988eb568b535a40
Scott Tustison <scott.tustison@gmail.com>
2015-02-23 15:58:25 -0700

    DDF-1062 fixed polygon drawing
    - removed select interaction
    - fixed selecting points inside polygons
    - fixed bounding box fields reverting to lat lon when usng is selected
    - fixed selection animation to mirror that of cesium
    - added codice fork of openlayers3

--- search-ui/standard/src/main/webapp/js/controllers/openlayers.controller.js
+++ search-ui/standard/src/main/webapp/js/controllers/openlayers.controller.js
@@ -85,8 +85,6 @@ define(['underscore',
                     });
 
                     map = new ol.Map({
-                        interactions: ol.interaction.defaults().extend(
-                            [new ol.interaction.Select()]),
                         layers: layers,
                         target: mapDivId,
                         view: new ol.View({
@@ -107,13 +105,18 @@ define(['underscore',
             setupEvents: function () {
                 var controller = this;
                 this.mapViewer.on('click', function(event) {
-                    var feature = controller.mapViewer.forEachFeatureAtPixel(event.pixel,
-                        function(feature) {
-                            return feature;
-                        });
-                    if (feature) {
-                        controller.trigger('click:left', feature);
-                    }
+                    var feature;
+
+                    controller.mapViewer.forEachLayerAtPixel(event.pixel, function (layer) {
+                        feature = controller.mapViewer.forEachFeatureAtPixel(event.pixel,
+                            function(feature) {
+                                controller.trigger('click:left', feature);
+                            }, this, function(testLayer) {
+                                if (testLayer === layer) {
+                                    return true;
+                                }
+                            });
+                    });
                 });
             },
 
